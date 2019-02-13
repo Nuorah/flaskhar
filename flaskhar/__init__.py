a71efcd8178e0eac3443ac344cdfc9b215156453
Haroun Moatassime <haroun.moatassime@gmail.com>
2019-02-13 10:33:26 +0100

    Config secret key

--- flaskhar/__init__.py
+++ flaskhar/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from .home import home
 from .pow import pow
+from .config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 app.register_blueprint(home, url_prefix='/')
 app.register_blueprint(pow, url_prefix='/pow')
