5f915270a6f304cdaebf8aa93c12b1fc25e28de6
Nuorah <haroun.moatassime@gmail.com>
2019-02-17 10:20:25 +0100

    ajout de proofOfWork sur la page des projets

--- flaskhar/pow/__init__.py
+++ flaskhar/pow/__init__.py
@@ -3,8 +3,8 @@ from flask import Blueprint
 pow = Blueprint(
     'pow',
     __name__,
-    template_folder='./templates',
-    static_folder="static",
+    template_folder='../templates',
+    static_folder="../static",
 )
 
 from . import routes
--- flaskhar/pow/forms.py
+++ flaskhar/pow/forms.py
@@ -4,7 +4,7 @@ from wtforms import validators
 from wtforms.validators import DataRequired, NumberRange
 
 class PowForm(FlaskForm):
-    block = StringField('Block', [validators.DataRequired("Texte requis")])
-    difficulty = IntegerField('Difficulty', [validators.NumberRange(1, 16, "Entier entre 1 et 16 requis"),
-                                           validators.DataRequired("Entier entre 1 et 16 requis")])
+    block = StringField('Block', [validators.DataRequired("Needs text.")])
+    difficulty = IntegerField('Difficulty', [validators.NumberRange(1, 16, "Needs an integer between 1 and 16."),
+                                           validators.DataRequired("Needs an integer between 1 and 16.")])
     submit = SubmitField('Mine')
